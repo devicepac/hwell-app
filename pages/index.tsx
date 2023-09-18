b352ccf2cf28dd87924c540d9e6e37aa6d837346
devicepac <bardichedevice@gmail.com>
2023-09-18 10:48:07 +0900

    헤더 1차 작업

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
+import Header from '@/components/common/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
+      <Header/>
       <main>안녕하세요</main>
     </>
   );
