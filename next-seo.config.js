ba7579bcdcd4ba18621c3b01612b5d4f6d28c3a0
devicepac <bardichedevice@gmail.com>
2023-09-20 11:58:12 +0900

    Feat:구글서치추가

--- next-seo.config.js
+++ next-seo.config.js
@@ -19,5 +19,9 @@ export default {
       name: 'naver-site-verification',
       content: '3c0e7e5b08610dddbbdc1915cb87c38e1776e59b',
     },
+    {
+      name: 'google-site-verification',
+      content: 'MVwcmLk-2gF9J1Xey2ooCPYU4PeBjjAcHzWJ9N42ML0',
+    },
   ],
 };
